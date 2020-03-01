051e905e04a3f7eb132ca1d4c894d92d4d41df87
Kengo <funeace@gmail.com>
2020-03-01 22:57:54 +0900

    Edit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,12 +3,23 @@ class BookCommentsController < ApplicationController
       @book_detail = Book.find(params[:book_id])
       @book_comment = current_user.book_comments.new(book_comment_params)
       @book_comment.book_id = @book_detail.id
-      @book_comment.save
-      redirect_back(fallback_location: root_path)
+    if @book_comment.save
+      redirect_to(book_path(@book_detail))
+    else
+      @book_comments = @book_detail.book_comments.all
+      @user = @book_detail.user
+      @book = Book.new
+      render '/books/show'
+    end
   end
   
   def destroy
-    BookComment.find(params[:book_id]).destroy
+    comment = BookComment.find(params[:book_id])
+
+# 処理を書く
+    if comment.user.id == current_user.id
+      comment.destroy
+    end
     redirect_back(fallback_location: root_path)
   end
 
